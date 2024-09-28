f97ac740b543bfd2cf83b4b367eda3a7ead1f2bf
NBK <nguyenkhiemit02@gmail.com>
2024-09-28 23:52:29 +0700

    fix the error and got the kafka running correctly

--- licensing-service/src/main/java/com/thoughtmechanix/licenses/Application.java
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/Application.java
@@ -1,12 +1,16 @@
 package com.thoughtmechanix.licenses;
 
+import com.thoughtmechanix.licenses.events.consumers.OrganizationChangeConsumer;
+import com.thoughtmechanix.licenses.events.models.OrganizationChangeModel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableKafka
 public class Application {
     @LoadBalanced
     @Bean
@@ -17,4 +21,9 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+//    @Bean
+//    public OrganizationChangeConsumer orgChangeConsumer() {
+//        return new OrganizationChangeConsumer();
+//    }
 }
--- licensing-service/src/main/java/com/thoughtmechanix/licenses/events/consumers/OrganizationChangeConsumer.java
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/events/consumers/OrganizationChangeConsumer.java
@@ -1,20 +1,38 @@
 package com.thoughtmechanix.licenses.events.consumers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.thoughtmechanix.licenses.events.models.OrganizationChangeModel;
 import java.util.function.Consumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
 @Component
-public class OrganizationChangeConsumer {
+public class OrganizationChangeConsumer{
   private static final Logger logger = LoggerFactory.getLogger(OrganizationChangeConsumer.class);
 
+  private final ObjectMapper objectMapper;
+
+  public OrganizationChangeConsumer(ObjectMapper objectMapper) {
+    this.objectMapper = objectMapper;
+  }
+
   @Bean
-  public Consumer<OrganizationChangeModel> input() {
-    return orgChange -> {
-      logger.debug("Received an event for organization id {}", orgChange.getOrganizationId());
+  public Consumer<Message<byte[]>> consume() {
+    return message -> {
+      try {
+        // Deserialize the byte[] payload into OrganizationChangeModel
+        OrganizationChangeModel orgChange = objectMapper.readValue(message.getPayload(), OrganizationChangeModel.class);
+
+        // Handle the deserialized object (log it, process it, etc.)
+        System.out.println("Received Organization Change Event: " + orgChange.getOrganizationId());
+        // Further processing...
+        logger.debug("Received an event for organization id {}", orgChange.getOrganizationId());
+      } catch (Exception e) {
+        throw new RuntimeException("Failed to deserialize message payload", e);
+      }
     };
   }
 }
